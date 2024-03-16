7cf5efe5f08330d1f8628616f08c4c3fd7a2fb41
no-regret666 <RangelJara195@gmail.com>
2024-03-16 20:56:16 +0800

    update shell

--- plan4/my_shell.c
+++ plan4/my_shell.c
@@ -5,97 +5,130 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <pwd.h>
+#include <time.h>
+#include <fcntl.h>
 
-#define MAXARGS 20
+
+#define MAXARGS 50
 #define ARGLEN 100
 
-char *readcmd(char *pathname);  // 读取命令行
-char *my_getcwd(); // 获取当前工作目录
-void execute(char *arglist[]);  // 执行命令
-int cd(char *path, char *pathname);
-void print_prompt(char *path);
+void set_prompt(char *prompt);        // 获取命令提示符
+void execute(int argc, char *argv[]); // 执行命令
+int cd(char *path, char *pathname);   // cd命令
+void redirect_input(char *input);    // 输入重定向
+void redirect_output(char *output);   // 输出重定向
+void find(int argc, char *argv[]);     // 寻找特殊符号
 
 char formerpath[256]; // 上一次工作目录
 
 int main()
 {
-    char *arglist[MAXARGS + 1], *cmdline, pathname[256];
-    strcpy(pathname,my_getcwd());
-    while ((cmdline = readcmd(pathname)) != NULL)
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    while (1)
     {
-        arglist[0] = cmdline;
+        char *argv[MAXARGS + 1], *cmdline, pathname[256], prompt[100];
+        int argc = 0;
+        getcwd(pathname, sizeof(pathname));
+        set_prompt(prompt);
+        if (!(cmdline = readline(prompt)))
+            break;
+        add_history(cmdline);
+
+        argv[0] = cmdline;
         for (int i = 0; i < MAXARGS; i++)
+            argv[i] = strtok_r(argv[i], " ", &argv[i + 1]);
+        while (argv[argc])
+            argc++;
+
+        if (argc == 0)
         {
-            arglist[i] = strtok_r(arglist[i], " ", &arglist[i + 1]);
+            free(cmdline);
+            continue;
         }
-        if (!strcmp(arglist[0], "cd"))
+        if (!strcmp(argv[0], "exit"))
         {
-            cd(arglist[1], pathname);
+            free(cmdline);
+            break;
+        }
+        if (!strcmp(argv[0], "cd"))
+        {
+            if (argc == 1)
+                argv[1] = "~";
+            cd(argv[1], pathname);
         }
         else
-        {
-            if (cmdline != NULL)
-            {
-                execute(arglist);
-            }
-        }
-        strcpy(pathname,my_getcwd());
+            execute(argc, argv);
         free(cmdline);
     }
     return 0;
 }
 
-char *readcmd(char *pathname)
+void set_prompt(char *prompt)
 {
-    print_prompt(pathname);
-    char *buf;
-    int bufsize = 0;
-    int pos = 0;
-    char c;
-    while ((c = getc(stdin)) != '\n')
+    // 获取主机名
+    char hostname[100];
+    if (gethostname(hostname, sizeof(hostname)))
     {
-        if (pos == 0)
-            buf = (char *)malloc(1);
-        else
-        {
-            buf = (char *)realloc(buf, bufsize + 2);
-        }
-        buf[pos++] = c;
-        bufsize++;
+        perror("gethostname");
+        exit(EXIT_FAILURE);
     }
-    if (pos == 0)
-        return NULL;
-    buf[pos] = '\0';
-    return buf;
-}
-
-char *my_getcwd()
-{
-    char *buf = getcwd(NULL, 0);
-    return buf;
-}
-
-void execute(char *arglist[])
-{
-    pid_t pid = fork();
-    switch (pid)
+    // 获取用户名
+    struct passwd *user;
+    char *username;
+    user = getpwuid(getuid());
+    username = user->pw_name;
+    // 获取当前工作目录
+    char cwd[100];
+    char buf[100];
+    getcwd(buf, sizeof(buf));
+    char *home = getenv("HOME");
+    size_t len = strlen(home);
+    if (!strncmp(buf, home, len))
     {
-    case -1:
+        strcpy(cwd, "~");
+        strcat(cwd, buf + len);
+    } // 将home目录改成~
+    // 获取时间
+    time_t curtime;
+    time(&curtime);
+    char time[10];
+    strncpy(time, ctime(&curtime) + 11, 8);
+
+    sprintf(prompt, "\n\e[40;33m %s@%s \e[0m\e[44;30m %s \e[0m\e[47;30m %s \e[0m\n\e[32m %s \e[0m",
+            username, hostname, cwd, time, "$");
+}
+
+void execute(int argc, char *argv[])
+{
+    if (!strcmp(argv[0], "ls"))
+    {
+        int index = 1;
+        if (argv[1])
+            index = 2;
+        argv[index++] = (char *)"--color=auto";
+    } // 给ls命令上色
+    pid_t pid = fork();
+    if (pid == -1)
+    {
         perror("fork");
         exit(EXIT_FAILURE);
-
+    }
     // 子进程
-    case 0:
-        execvp(arglist[0], arglist);
+    if (pid == 0)
+    {
+        find(argc,argv);
+        execvp(argv[0], argv);
         perror("execvp");
         exit(EXIT_FAILURE);
-
-    // 父进程等待子进程退出
-    case 1:
-        int child_status;
-        if ((wait(&child_status)) == -1)
-            perror("wait");
     }
+    // 父进程等待子进程退出
+    int status = 0;
+    pid_t ret = waitpid(pid, &status, 0);
 }
 
 int cd(char *targetpath, char *pathname)
@@ -105,31 +138,54 @@ int cd(char *targetpath, char *pathname)
     else
         strcpy(formerpath, pathname);
 
-    if(!strncmp(targetpath,"~",1)){
+    if (!strncmp(targetpath, "~", 1))
+    {
         char *home = getenv("HOME");
         char path[256];
-        strcpy(path,home);
-        strcat(path,targetpath + 1);
+        strcpy(path, home);
+        strcat(path, targetpath + 1);
         targetpath = path;
-    }
+    } // chdir无法识别~,需将~改为home路径
 
     if (chdir(targetpath) == -1)
-    {
         perror("chdir");
+}
+
+void redirect_input(char *input)
+{
+    int fd,newfd;
+    fd = open(input,O_RDONLY);
+    newfd = dup2(fd,0);
+    if(newfd != 0){
+        fprintf(stderr,"Could not open data as fd 0\n");
+        exit(1);
     }
 }
 
-void print_prompt(char *pathname)
+void redirect_output(char *output)
 {
-    char *buf = pathname;
-    char path[256];
-    char *home = getenv("HOME");
-    size_t len = strlen(home);
-    if (!strncmp(buf, home, len))
+    int fd,newfd;
+    fd = open(output,O_WRONLY | O_CREAT | O_TRUNC);
+    newfd = dup2(fd,1);
+    if(newfd != 1){
+        fprintf(stderr,"Could not open data as fd 1\n");
+        exit(1);
+    }
+}
+
+void find(int argc, char *argv[])
+{   
+    for (int i = 0; i < argc; i++)
     {
-        strcpy(path, "~");
-        strcat(path, buf + len);
-    } // 将home目录改成~
-    fprintf(stdout, "\e[40;33m %s \e[0m\e[44;30m %s \e[0m\e[30;37m %s \e[0m",
-    "noregret@noregret-arch",path,"$");
+        if (!strcmp(argv[i], "<")){
+            redirect_input(argv[i + 1]);
+            argv[i] = NULL;
+            i++;
+        }
+        if(!strcmp(argv[i],">")){
+            redirect_output(argv[i + 1]);
+            argv[i] = NULL;
+            i++;
+        }
+    }
 }
--- plan4/test.c
+++ plan4/test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    printf("%s",getenv("HOME"));
+    printf("%d","\n" == NULL);
 }
