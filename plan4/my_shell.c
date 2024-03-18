79b5258c83bdb7ee4a04b83b0c94eb1ef89fed24
no-regret666 <RangelJara195@gmail.com>
2024-03-18 23:09:12 +0800

    update shell

--- plan4/my_shell.c
+++ plan4/my_shell.c
@@ -10,45 +10,50 @@
 #include <pwd.h>
 #include <time.h>
 #include <fcntl.h>
+#include <asm-generic/siginfo.h>
 
 #define MAXARGS 50
 #define ARGLEN 100
 
 void set_prompt(char *prompt);               // 获取命令提示符
-void execute(int argc, char *argv[]);        // 执行命令
 int cd(char *path, char *pathname);          // cd命令
 void redirect_input(char *input);            // 输入重定向
 void redirect_output(char *output, int add); // 输出重定向
-void redirect(int argc, char *argv[]);       // 寻找特殊符号
-void excute_pipe(char *cmdline);
+void find(int argc, char *argv[]);       // 寻找特殊符号
+void excute(char *cmdline);                  // 执行命令
+void sigchld_handler(int sig,siginfo_t *info,void *context);  //SIGCHLD信号处理函数
 
 char formerpath[256]; // 上一次工作目录
 
 int main()
 {
+    // 屏蔽信号
     signal(SIGINT, SIG_IGN);
     signal(SIGQUIT, SIG_IGN);
     signal(SIGTSTP, SIG_IGN);
     while (1)
     {
-        char *argv[MAXARGS + 1], *cmdline, pathname[256], prompt[100];
+        char *argv[MAXARGS + 1], *cmdline, pathname[256], prompt[150];
         int argc = 0;
         getcwd(pathname, sizeof(pathname));
         set_prompt(prompt);
+        fflush(stdin);
         if (!(cmdline = readline(prompt)))
             break;
         add_history(cmdline);
 
+        char buf[ARGLEN];
+        strcpy(buf, cmdline);
         argv[0] = cmdline;
-        for (int i = 0; i < MAXARGS; i++)
+        for (int i = 0; i < MAXARGS - 1; i++)
             argv[i] = strtok_r(argv[i], " ", &argv[i + 1]);
         while (argv[argc])
             argc++;
 
-        int pipe_num = 0;
+        int pipe_num = 0, back = 0;
         for (int i = 0; i < argc; i++)
         {
-            if (strcmp(argv[i], "|"))
+            if (!strcmp(argv[i], "|"))
                 pipe_num++;
         }
         if (argc == 0)
@@ -69,10 +74,7 @@ int main()
         }
         else
         {
-            if (pipe_num == 0)
-                execute(argc, argv);
-            else
-                excute_pipe(cmdline);
+            excute(buf);
         }
         free(cmdline);
     }
@@ -88,11 +90,13 @@ void set_prompt(char *prompt)
         perror("gethostname");
         exit(EXIT_FAILURE);
     }
+
     // 获取用户名
     struct passwd *user;
     char *username;
     user = getpwuid(getuid());
     username = user->pw_name;
+
     // 获取当前工作目录
     char cwd[100];
     char buf[100];
@@ -104,6 +108,7 @@ void set_prompt(char *prompt)
         strcpy(cwd, "~");
         strcat(cwd, buf + len);
     } // 将home目录改成~
+
     // 获取时间
     time_t curtime;
     time(&curtime);
@@ -112,34 +117,6 @@ void set_prompt(char *prompt)
 
     sprintf(prompt, "\n\e[40;33m %s@%s \e[0m\e[44;30m %s \e[0m\e[47;30m %s \e[0m\n\e[32m %s \e[0m",
             username, hostname, cwd, time, "$");
-}
-
-void execute(int argc, char *argv[])
-{
-    if (!strcmp(argv[0], "ls"))
-    {
-        int index = 1;
-        if (argv[1])
-            index = 2;
-        argv[index++] = (char *)"--color=auto";
-    } // 给ls命令上色
-    pid_t pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    // 子进程
-    if (pid == 0)
-    {
-        find(argc, argv);
-        execvp(argv[0], argv);
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    }
-    // 父进程等待子进程退出
-    int status = 0;
-    pid_t ret = waitpid(pid, &status, 0);
 }
 
 int cd(char *targetpath, char *pathname)
@@ -189,7 +166,7 @@ void redirect_output(char *output, int add)
     }
 }
 
-void redirect(int argc, char *argv[])
+void find(int argc, char *argv[])
 {
     for (int i = 0; i < argc; i++)
     {
@@ -211,67 +188,126 @@ void redirect(int argc, char *argv[])
             argv[i] = NULL;
             i++;
         }
+        if (!strcmp(argv[i], "&"))
+        {
+            
+        }
     }
 }
 
-void excute_pipe(char *cmdline)
+void excute(char *cmdline)
 {
     // 按 | 分割命令
     int cmd_num = 0;
     char *cmd[MAXARGS];
-    char *token = strtok(cmdline, "|");
-    while (token)
+    cmd[0] = cmdline;
+    for (int i = 0; i < MAXARGS; i++)
+        cmd[i] = strtok_r(cmd[i], "|", &cmd[i + 1]);
+    while (cmd[cmd_num])
+        cmd_num++;
+    int pipe_num = cmd_num - 1;
+
+    // 建立管道
+    int pipefd[pipe_num][2];
+    for (int i = 0; i < pipe_num; i++)
     {
-        cmd[cmd_num++] = token;
-        token = strtok(NULL, "|");
+        if (pipe(pipefd[i]) == -1)
+        {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    int pipefd[2];
-    if (pipe(pipefd) == -1)
-    {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
+    pid_t pid;
     for (int i = 0; i < cmd_num; i++)
     {
-        pid_t pid = fork();
-        if (pid == -1)
+        if ((pid = fork()) == -1)
         {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        else if (pid == 0)
+
+        // 子进程
+        if (pid == 0)
         {
-            if (i > 0)
+            if (pipe_num)
             {
-                close(pipefd[0]);
-                if (dup2(pipefd[1], 1) == -1)
+                // 第一个命令
+                if (i == 0)
                 {
-                    fprintf(stderr, "Could not redirect stdout");
-                    exit(EXIT_FAILURE);
+                    // 关闭读数据端，并重定向写数据端
+                    close(pipefd[i][0]);
+                    dup2(pipefd[i][1], 1);
+                    close(pipefd[i][1]);
                 }
-                close(pipefd[1]);
+
+                // 最后一个命令
+                else if (i == cmd_num - 1)
+                {
+                    // 关闭读数据端，并重定向写数据端
+                    close(pipefd[i - 1][1]);
+                    dup2(pipefd[i - 1][0], 0);
+                    close(pipefd[i - 1][0]);
+                }
+
+                // 中间的命令
+                else
+                {
+                    // 重定向读数据端
+                    close(pipefd[i - 1][1]);
+                    dup2(pipefd[i - 1][0], 0);
+                    close(pipefd[i - 1][0]);
+                    // 重定向写数据端
+                    close(pipefd[i][0]);
+                    dup2(pipefd[i][1], 1);
+                    close(pipefd[i][1]);
+                }
+
+                // 关闭所有管道
+                for (int i = 0; i < pipe_num; i++)
+                {
+                    close(pipefd[i][0]);
+                    close(pipefd[i][1]);
+                }
             }
 
-            if (i < cmd_num - 1)
+            char *argv[MAXARGS];
+            int argc = 0;
+            argv[0] = cmd[i];
+            printf("%s", cmd[i]);
+            for (int j = 0; j < MAXARGS - 1; j++)
+                argv[j] = strtok_r(argv[j], " ", &argv[j + 1]);
+            while (argv[argc])
             {
-                int new_pipefd[2];
-                if (pipe(pipefd) == -1)
-                {
-                    perror("pipe");
-                    exit(EXIT_FAILURE);
-                }
-                close(pipefd[1]);
-                if(dup2(pipefd[0],0) == -1){
-                    fprintf(stderr, "Could not redirect stdin");
-                    exit(EXIT_FAILURE);
-                }
-                close(pipefd[0]);
-                pipefd[0] = new_pipefd[0];
-                pipefd[1] = new_pipefd[1];
-
-                //执行当前命令
+                printf("%s", argv[argc]);
+                argc++;
             }
+
+            find(argc, argv);
+            if (!strcmp(argv[0], "ls"))
+            {
+                int index = 1;
+                if (argv[1])
+                    index = 2;
+                argv[index++] = (char *)"--color=auto";
+            } // 给ls命令上色
+            execvp(argv[0], argv);
+            perror("execvp");
+            exit(EXIT_FAILURE);
         }
     }
+
+    // 父进程
+    for (int i = 0; i < pipe_num; i++)
+    {
+        close(pipefd[i][0]);
+        close(pipefd[i][1]);
+    }
+    for (int i = 0; i < cmd_num; i++)
+        wait(NULL);
 }
+
+// void sigchld_handler(int sig,siginfo_t *info,void *context){
+//     pid_t child_pid = info->si_code;
+//     int child_status = info->
+// }
