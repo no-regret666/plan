80c9bd3d116bbecfd740496ba95906baaf0f0b67
no-regret666 <RangelJara195@gmail.com>
2024-05-06 19:56:56 +0800

    finish sort

--- plan5/Sort.c
+++ plan5/Sort.c
@@ -6,7 +6,13 @@
 #include <string.h>
 
 #define TOTAL_NUM 1000
-pthread_mutex_t mutex;
+int nums[1000];
+int sortedNums[1000];
+//要排序的数组信息
+typedef struct{
+    int startIndex; //数组起始下标
+    int num; //要排序的数量
+}SortInfo_t;
 
 //qsort比较函数
 int compare(const void* num1,const void* num2){
@@ -17,16 +23,36 @@ int compare(const void* num1,const void* num2){
 
 //线程需要实现的功能
 static void* threadFunc(void *arg){
-    pthread_mutex_lock(&mutex);
-    int* info = (int*)arg;
-    qsort(info,sizeof(info)/4,sizeof(int),compare);
+    SortInfo_t* sortInfo = (SortInfo_t*)arg;
+    int index = sortInfo->startIndex;
+    int num = sortInfo->num;
+    qsort(&nums[index],num,sizeof(int),compare);
+}
+
+//合并排序
+void merge(SortInfo_t* sortInfos,int threadNum){
+    int index[threadNum];
+    for(int i = 0;i < threadNum;i++){
+        index[i] = sortInfos[i].startIndex;
+    }
+    for(int i = 0;i < 1000;i++){
+        int minIndex = -1;
+        int minValue = INT_MAX;
+        for(int j = 0;j < threadNum;j++){
+            if(index[j] - sortInfos[j].startIndex < sortInfos[j].num && nums[index[j]] < minValue){
+                minIndex = j;
+                minValue = nums[index[j]];
+            }
+        }
+            sortedNums[i] = minValue;
+            index[minIndex]++;
+    }
 }
 
 int main(int argc,char *argv[]){
-    int num[1000];
     srand((unsigned)time(NULL));
     for(int i = 0;i < TOTAL_NUM;i++){
-        num[i] = rand() % 100;
+        nums[i] = rand() % 100;
     }
 
     printf("请输入所需线程数量：\n");
@@ -44,35 +70,56 @@ int main(int argc,char *argv[]){
         lastNum = TOTAL_NUM % (threadNum - 1);
     }
 
-    //切分数组
-    int sortNum[TOTAL_NUM][TOTAL_NUM];
+    //分配排序信息
+    SortInfo_t* sortInfos = (SortInfo_t*)malloc(threadNum * sizeof(SortInfo_t));
+    memset(sortInfos,0,sizeof(SortInfo_t) * threadNum);
     for(int i = 0;i < threadNum;i++){
         if(i != threadNum - 1){
-            memcpy(sortNum[i],num,perNum);
+            sortInfos[i].startIndex = i * perNum;
+            sortInfos[i].num = perNum;
         }
         else{
-            memcpy(sortNum[i],num,lastNum);
+            sortInfos[i].startIndex = i * perNum;
+            sortInfos[i].num = lastNum;
         }
     }
 
     //记录时间
-    int begin,end;
-    begin = clock();
+    struct timeval start,end;
+    long long start_usec,end_usec;
+    double total;
+    gettimeofday(&start,NULL);
 
     //创建线程
-    pthread_t pid;
+    pthread_t* pids = (pthread_t *)malloc(threadNum * sizeof(pthread_t));
     for(int i = 0;i < threadNum;i++){
-        err = pthread_create(&pid,NULL,threadFunc,sortNum[i]);
+        err = pthread_create(&pids[i],NULL,threadFunc,(void*)(&sortInfos[i]));
         if(err != 0){
             perror("pthread_create");
             exit(EXIT_FAILURE);
         }
     }
 
+    //等待所有线程完成
+    for(int i = 0;i < threadNum;i++){
+        err = pthread_join(pids[i],NULL);
+        if(err != 0){
+            perror("pthread_join");
+            exit(EXIT_FAILURE);
+        }
+    }
+    merge(sortInfos,threadNum);
+
     for(int i = 0;i < 1000;i++){
-        printf("%d ",sortNum[0][i]);
+        printf("%d ",sortedNums[i]);
     }
 
-    end = clock();
-    printf("%d个线程:%dms\n",threadNum,end - begin);
+    gettimeofday(&end,NULL);
+    start_usec = start.tv_sec * 1000000 + start.tv_usec;
+    end_usec = end.tv_sec * 1000000 + end.tv_usec;
+    total = (double)(end_usec - start_usec)/1000000.0;
+    printf("%d个线程:%fs\n",threadNum,total);
+
+    free(sortInfos);
+    free(pids);
 }
